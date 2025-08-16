4dc9e9ec213f19ea6e33de1fc6b87fec383e7667
Sudhang-Balisseri <balisseri@gmail.com>
2025-08-16 16:12:54 +0530

    conditional-statements

---
+++ JAVASCRIPT/Conditional-Statements /Conditional-Statements.js
@@ -0,0 +1,143 @@
+// *******************************************
+
+//if-statement
+
+// *******************************************
+
+//let age = 26;
+
+// if (age > 18)
+// {
+//     console.log("You can vote");
+// }
+
+// if (age < 18)
+// {
+//     console.log("You CAN'T vote!");
+// }
+
+// *********************************************
+
+
+// let mode = "light";
+// let color;
+
+// if (mode === "dark")
+// {
+//     color = "black";
+// }
+
+// if (mode === "light")
+// {
+//     color = "white";
+// }
+
+// console.log(color);
+
+
+// ************************************************
+
+//if-else-statement
+
+// ************************************************
+//odd or even
+
+// let num = 5;
+
+// if (num%2 === 0)
+// {
+//     console.log(num, "is a even number.")
+// }
+// else
+// {
+//     console.log(num, "is NOT a even number!")
+// }
+
+// ************************************************
+
+//else-if-statement
+
+// ************************************************
+
+// let age = 69;
+
+// if (age < 18)
+// {
+//     console.log("junior")
+// }
+// else if (age > 60)
+// {
+//     console.log("senior")
+// }
+// else{
+//     console.log("middle")
+// }
+
+// ************************************************
+
+//ternary-operators
+
+// ************************************************
+
+// let age = 69;
+
+// let isAdult = age > 18 ? "adult" : "not adult";
+// console.log(isAdult);
+
+// ************************************************
+
+//practice
+
+// ************************************************
+
+/*Qs1. Get user to input a number using prompt(“Enter a number:”). Check if the number is
+a multiple of 5 or not.*/
+
+// *************
+
+// let num = prompt("Enter a number:");
+
+// if (num%5 === 0)
+// {
+//     console.log(num, "is multiple of 5")
+// }
+// else
+// {
+//     console.log(num, "is NOT multiple of 5!")
+// }
+
+// ***********************************************
+
+// Qs2. Write a code which can give grades to students according to their scores:
+// 80-100, A
+// 70-89, B
+// 60-69, C
+// 50-59, D
+// 0-49, F
+
+// ************
+
+// let scores = 69;
+
+// if (scores > 80)
+// {
+//     console.log("Grade A")
+// }
+// else if (scores >= 70 && scores <= 89)
+// {
+//     console.log("Grade = B")
+// }
+// else if (scores >= 60 && scores <= 69)
+// {
+//     console.log("Grade C")
+// }
+// else if (scores >= 50 && scores <= 59)
+// {
+//     console.log("Grade D")
+// }
+// else
+// {
+//     console.log("Grade F")
+// }
+
+// *************************************************
