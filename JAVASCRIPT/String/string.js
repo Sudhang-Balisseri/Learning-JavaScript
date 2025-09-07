a7564f21b2889a7ec6a28be20ce2c813af11f0f3
Sudhang-Balisseri <balisseri@gmail.com>
2025-09-07 14:34:54 +0530

    string-running

---
+++ JAVASCRIPT/String/string.js
@@ -0,0 +1,43 @@
+// *******************************************
+// basic of string
+// ******************************************
+
+// let str = "leg";//String
+
+// console.log(str.length);//String Length
+
+// for(let i=2; i>=0; i--)
+// {
+//     console.log(str[i]);//String Indices
+// }
+
+// **********************************************
+// Template Literals
+// **********************************************
+// let object = {
+//     product : "leg",
+//     price : 10,
+// };
+
+// let str = `the ${object.product} cost ${object.price}Rs`;;
+
+
+// console.log(str)
+// console.log(typeof(str));
+
+// ************************************************
+// String Methods
+// ************************************************
+
+// let str = "lEg";
+// let str2 = "    LEG   ";  
+
+// console.log(str.toUpperCase());
+// console.log(str.toLocaleLowerCase());
+// console.log(str2.trim());// removes whitespaces
+// console.log(str.slice(1, 4));// returns part of string
+// console.log(str.concat(str2));//joins str2 wiht str
+// console.log(str.replace("l", "E"));
+
+
+
